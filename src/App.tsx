0cd12fcdebb8e937441dcb1e8bef38646a8b2432
Scott Holmes <scott.holmes44@gmail.com>
2024-02-04 21:50:57 -0800

    chore: put board, keyboard and actions into an app-layout file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import React from "react"
-import { SudokuProvider } from "./context/app-context"
-import Board from "./ui/board"
-import Actions from "./ui/actions"
 import { ToastContainer } from "react-toastify"
-import NumericKeypad from "./ui/numeric-keypad"
-import { Grid } from "@mui/material"
+import AppLayout from "./ui/app-layout"
+import { Container } from "@mui/material"
 
 function App() {
   return (
-    <>
+    <Container maxWidth={"md"}>
       <ToastContainer position="top-center" />
-      <SudokuProvider>
-        <Grid container direction={"row"}>
-          <Grid item md={6} xs={12}>
-            <Board />
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <Actions />
-            <NumericKeypad />
-          </Grid>
-        </Grid>
-      </SudokuProvider>
-    </>
+      <AppLayout />
+    </Container>
   )
 }
 
---
+++ src/ui/app-layout.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import Board from "./board"
+import Actions from "./actions"
+import NumericKeypad from "./numeric-keypad"
+import { Grid, Typography } from "@mui/material"
+import { SudokuProvider } from "../context/app-context"
+
+export default function AppLayout() {
+  return (
+    <SudokuProvider>
+      <Grid
+        container
+        direction={"row"}
+        alignItems={"center"}
+        minHeight={"75vh"}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h2" textAlign={"center"}>
+            Webdoku
+          </Typography>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Board />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Actions />
+          <NumericKeypad />
+        </Grid>
+      </Grid>
+    </SudokuProvider>
+  )
+}
