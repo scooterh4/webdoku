16db692a42e008faaea39b1bdfaf598cf3565af2
Scott Holmes <scott.holmes44@gmail.com>
2024-02-11 22:22:47 -0800

    add MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,66 @@
 import { ToastContainer } from "react-toastify"
 import AppLayout from "./ui/app-layout"
-import { Container } from "@mui/material"
+import { Container, ThemeProvider, createTheme } from "@mui/material"
 import { SudokuProvider } from "./context/app-context"
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Plus Jakarta Sans, arial, sans-serif",
+    h1: {
+      fontWeight: 700,
+      fontSize: "5rem",
+    },
+    h2: {
+      fontSize: "3rem",
+    },
+    h3: {
+      fontSize: "2.5rem",
+      fontWeight: 500,
+    },
+    button: {
+      letterSpacing: ".5px",
+      color: "#EAE0D5",
+    },
+    subtitle2: {
+      color: "#888888",
+    },
+  },
+  palette: {
+    primary: {
+      main: "#4F60AE",
+    },
+    secondary: {
+      main: "#AE4F60",
+    },
+    success: {
+      main: "#60AE4F",
+      contrastText: "#ffffff",
+    },
+    background: {
+      paper: "#EAE0D5",
+      default: "#EAE0D5",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+    },
+  },
+})
 
 function App() {
   return (
     <Container maxWidth={"md"}>
-      <ToastContainer position="top-center" theme="colored" />
-      <SudokuProvider>
-        <AppLayout />
-      </SudokuProvider>
+      <ThemeProvider theme={theme}>
+        <ToastContainer position="top-center" theme="colored" />
+        <SudokuProvider>
+          <AppLayout />
+        </SudokuProvider>
+      </ThemeProvider>
     </Container>
   )
 }
